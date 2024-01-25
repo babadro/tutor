2130d19f4436bdcd8df2bbcdbd92e029d44b0591
Andrei <babadro@gmail.com>
2024-01-25 07:01:49 +0100

    got the message from the llm

--- internal/infra/restapi/handlers/tutor/tutor.go
+++ internal/infra/restapi/handlers/tutor/tutor.go
@@ -2,13 +2,11 @@ package tutor
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/babadro/tutor/internal/infra/restapi/operations"
 	"github.com/babadro/tutor/internal/models"
 	"github.com/go-openapi/runtime/middleware"
-
-	hlog "github.com/rs/zerolog/hlog"
+	"github.com/rs/zerolog/hlog"
 )
 
 type service interface {
@@ -24,13 +22,14 @@ func NewTutor(svc service) *Tutor {
 }
 
 func (t *Tutor) SendChatMessage(params operations.SendChatMessageParams, principal *models.Principal) middleware.Responder {
-	fmt.Println("Got message from flutter: ", params.Body.Message)
+	if params.Body.Message == "" {
+		hlog.FromRequest(params.HTTPRequest).Error().Msg("Empty message")
+		return operations.NewSendChatMessageBadRequest()
+	}
 
 	reply, err := t.svc.SendMessage(params.HTTPRequest.Context(), params.Body.Message)
 	if err != nil {
 		hlog.FromRequest(params.HTTPRequest).Error().Err(err).Msg("Unable to send message")
-	} else {
-		fmt.Println("Reply from LLM: ", reply)
 	}
 
 	return operations.NewSendChatMessageOK().WithPayload(&operations.SendChatMessageOKBody{
