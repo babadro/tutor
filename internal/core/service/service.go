9ae72ded7bd216ac2a56ae71fa6405c30dcdba87
Andrei <babadro@gmail.com>
2024-01-28 13:58:33 +0100

    hardcoded urls for mp3 files

--- internal/core/service/service.go
+++ internal/core/service/service.go
@@ -27,8 +27,8 @@ func (s *Service) SendMessage(ctx context.Context, message string) (string, erro
 
 func (s *Service) SendVoiceMessage(ctx context.Context, voiceMsgFile []byte, userEmail string) (models.SendVoiceMessageResult, error) {
 	return models.SendVoiceMessageResult{
-		VoiceMessageURL:         "https://example.com/voice_message.wav",
-		VoiceResponseURL:        "https://example.com/voice_response.wav",
+		VoiceMessageURL:         "https://firebasestorage.googleapis.com/v0/b/tutor-fq8fmu.appspot.com/o/How-are-you.mp3?alt=media&token=b1339bfe-6cf8-44ae-bda5-6bb6dcb43c5b",
+		VoiceResponseURL:        "https://firebasestorage.googleapis.com/v0/b/tutor-fq8fmu.appspot.com/o/I-m-fine-thank-you.mp3?alt=media&token=ce525989-7c66-4ee3-8e21-68e4619fccc0",
 		VoiceMessageTranscript:  "Hello, how are you?",
 		VoiceResponseTranscript: "I'm fine, thank you.",
 	}, nil
