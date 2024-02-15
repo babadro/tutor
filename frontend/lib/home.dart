ce738e1f4041deffa12ed8aa890c4c51348b8a83
Andrei <babadro@gmail.com>
2024-02-15 08:54:28 +0100

    reorganizing gui

--- frontend/lib/home.dart
+++ frontend/lib/home.dart
@@ -6,6 +6,81 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    return LayoutBuilder(
+        builder: (context, constraints) {
+          return Scaffold(
+            appBar: AppBar(
+              actions: [
+                const SignOutButton(),
+                IconButton(
+                  icon: const Icon(Icons.person),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute<ProfileScreen>(
+                        builder: (context) => ProfileScreen(
+                          appBar: AppBar(
+                            title: const Text('User Profile'),
+                          ),
+                          actions: [
+                            SignedOutAction((context) {
+                              Navigator.of(context).pop();
+                            })
+                          ],
+                          children: [
+                            const Divider(),
+                            Padding(
+                              padding: const EdgeInsets.all(2),
+                              child: AspectRatio(
+                                aspectRatio: 1,
+                                child: Image.asset('flutterfire_300x.png'),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                )
+              ],
+              automaticallyImplyLeading: false,
+            ),
+            body: Row(
+              children: [
+                SafeArea(
+                  child: NavigationRail(
+                    extended: constraints.maxWidth >= 600,
+                    destinations: [
+                      NavigationRailDestination(
+                        icon: Icon(Icons.home),
+                        label: Text('Home'),
+                      ),
+                      NavigationRailDestination(
+                        icon: Icon(Icons.chat),
+                        label: Text('New Chat'),
+                      ),
+
+                    ],
+                    selectedIndex: 0,
+                    onDestinationSelected: (value) {
+
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Theme.of(context).colorScheme.primaryContainer,
+                    child: Placeholder(),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+    );
+
+    /*
     return Scaffold(
       appBar: AppBar(
         actions: [
@@ -44,16 +119,15 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('dash.png'),
-            Text(
-              'Welcome!',
-              style: Theme.of(context).textTheme.displaySmall,
-            ),
             const SignOutButton(),
+            // Include your listOfVerticalElements or Expanded ListView here
           ],
         ),
       ),
     );
+
+     */
   }
 }
