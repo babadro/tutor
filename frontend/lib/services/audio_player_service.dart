4c4a16404cf85cd33f599abd63a0a124d13a9ce8
Andrei <babadro@gmail.com>
2024-05-31 07:35:59 +0200

    just_audio player

--- frontend/lib/services/audio_player_service.dart
+++ frontend/lib/services/audio_player_service.dart
@@ -1,47 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:logger/logger.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class AudioPlayerService with ChangeNotifier {
-  FlutterSoundPlayer? _player;
+  AudioPlayer _player = AudioPlayer();
   String? _currentUrl;
 
-  AudioPlayerService() {
-    _player = FlutterSoundPlayer(logLevel: Level.info);
-    _player!.openPlayer();
-  }
-
   bool isPlaying(String url) {
-    return _currentUrl == url && _player!.isPlaying;
+    return _currentUrl == url;
   }
 
   void togglePlayPause(String url) async {
     print('togglePlayPause: $url');
-    if (_currentUrl == url && _player!.isPlaying) {
+    if (_currentUrl == url) {
       print('stopping player');
-      await _player!.stopPlayer();
-    } else {
-      print('starting player');
-      // log player
-      print('player: $_player');
-      await _player!.startPlayer(
-        fromURI: url,
-        whenFinished: (){
-          print('whenFinished');
-          _player!.stopPlayer();
-          _currentUrl = null;
-          notifyListeners();
-          },
-      );
+      await _player.stop();
+      _currentUrl = null;
+      notifyListeners();
+
+      return;
     }
 
+
+    print('starting player');
+    await _player.stop();
+    await _player.setUrl(url);
+
+    print('stopped player');
     _currentUrl = url;
+    notifyListeners();
+
+    await _player.play();
+    _currentUrl = null;
+
+
     notifyListeners();
   }
 
   @override
   void dispose() {
-    _player!.closePlayer();
+    _player.dispose();
     super.dispose();
   }
 }
